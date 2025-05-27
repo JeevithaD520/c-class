e58f9bb1156f600ceb3f43511c5d858b98bb1876
JeevithaD520 <djeevitha90@gmail.com>
2025-05-27 16:09:35 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float weight=70.5f;
+    float height=1.75f;
+
+    float bmi=weight/(height*height);
+    cout<<"BMI:"<<bmi<<endl;
+
+    return 0;
+}
---
+++ currency.converter.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float usd= 10.0f;
+    float inr= usd * 83.2f;
+
+    cout<<"INR:"<<inr<<endl;
+    return 0;
+}
